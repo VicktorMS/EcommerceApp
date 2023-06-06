06f7e0cc4f2557e9492dfcb114b49392d4d4f03d
Victor Moraes <victor.moraesrj@gmail.com>
2023-06-06 12:10:12 -0300

    Comments Screen

--- src/screens/RootProduct.js
+++ src/screens/RootProduct.js
@@ -2,13 +2,15 @@ import { StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import ProductMain from "./Product/ProductMain";
+import Comments from "./Product/Comments";
 
 const BottomTab = createBottomTabNavigator();
 
 export default function RootProduct({ navigation, route }) {
   return (
     <BottomTab.Navigator>
-      <BottomTab.Screen name="ProductMain" component={ProductMain} />
+      <BottomTab.Screen name="Produto" component={ProductMain} />
+      <BottomTab.Screen name="Comments" component={Comments} />
     </BottomTab.Navigator>
   );
 }
